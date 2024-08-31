e2327813f35f1a7644f225bf03a3e0e203569c43
yuichi.oba <yuichi.oba@sorich.jp>
2024-08-31 11:49:15 +0900

    ログインボタン押下時の処理の実装（暫定）

--- src/features/login/hooks/useInteract.ts
+++ src/features/login/hooks/useInteract.ts
@@ -1,28 +1,25 @@
 import { useForm } from "vee-validate"
 import * as schema from "../types/schema"
 import { toTypedSchema } from "@vee-validate/zod"
-import * as response from "@/shared/api/response"
 import { client } from "@/shared/api/client"
-
-export const handleSubmitError = (e: response.ApiSchema<"GeneralError">[]) => {
-  alert(JSON.stringify(e, undefined, 2))
-}
+import { handleSubmitError } from "./functions"
 
 export const useInteract = () => {
   const { handleSubmit, defineField, errors } = useForm<schema.LoginInputSchema>({
     validationSchema: toTypedSchema(schema.schema),
   })
 
-
   const onClickSignIn = handleSubmit(async (body) => {
-    console.log('hello')
     const { error } = await client.POST("/normal/login", { body })
     if (error) {
+      // TODO : 現状、alertにしているが、コールバック関数渡して、errorMessageを設定するか、モーダル出すか良しなにできるように数r
+      // この辺りは、細かいデザインが決まってないので後からにする
       handleSubmitError(error)
     }
+    // TODO : home画面への遷移を実装する
   })
 
   return {
-    handleSubmit, defineField, errors, onClickSignIn
+    defineField, errors, onClickSignIn
   }
 }
